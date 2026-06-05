00224b3e317b1f45bbf2dd43eb85ce238551a1b7
johnpork <133123652+ivan-kalivan@users.noreply.github.com>
2026-06-05 23:16:30 +1000

    добавил и прописал функции в config_manager.py

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -1,7 +1,29 @@
 import config
+import json
+import os
+import platform
+
+# возвращает строку полный путь к файлу config.json где будут храниться настройки пользователя
 def get_config_path():
-    print()
-def load_config():
-    print()
-def save_config():
-    print()
+    home_path = os.path.expanduser("~")
+    this_platform = platform.system()
+    if this_platform == "Windows":
+        config_dir = os.path.join(home_path, "AppData", "Roaming", "Sorter")
+    elif this_platform == "Darwin":
+        config_dir = os.path.join(home_path, "Library", "Application Support" "Sorter")
+    else:
+        config_dir = os.path.join(home_path, ".config", "sorter")
+    os.makedirs(config_dir, exist_ok=True)
+    return os.path.join(config_dir, "config.json")
+
+# возвращаем путь к файлу default_cfg.json из директории config
+def load_default_config_path():
+    this_path = os.path.dirname(__file__)
+    uplvl_path = os.path.dirname(this_path)
+    return os.path.join(uplvl_path, "config/default_cfg.json")
+
+#
+# def load_default_config(default_cfg_file):
+
+
+
